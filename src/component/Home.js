246b8d14b83a04d75a96b5cbefbcb1e43b90e5f7
widedboukouba <widedboukouba64@gmail.com>
2023-08-19 14:13:30 +0200

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+<div className="top">
+	<div className="coluumns">
+		<div className="coluumn is-full featured_wraapper p-0">
+			<img src="https://images.unsplash.com/photo-1517604931442-7e0c8ed2963c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80
+			" className="featured"/>
+			<div className="title_wraapper">
+				<span className="ttt">Trending Today</span>
+				<p  className="ttt">The Untold Story of the great Lorem Ipsum</p>
+				<button className='butt'>Watch It Now</button>
+			</div>
+		</div>
+	</div> </div>
+</div>
+
+  )
+}
+
+export default Home
